110d1a7540e4f09030120061d140d4892d67da57
Anurag Das <36499990-dasanurag88@users.noreply.replit.com>
2024-09-29 10:26:03 +0000

    commiting stage

--- escape_comments.py
+++ escape_comments.py
@@ -3,6 +3,7 @@ print("First Print Statement")
 print("Second Print Statement")
 print("First Print Statement", end="")
 print("Second Print Statement")
+print("Mark","Henry",45,sep="-")
 
 
 #Singe Line Comment
@@ -13,4 +14,12 @@ Using
 Multi Line Comment
 """
 print("Python does not have multiline comment but it can be achieved through DOCSTRINGS using triple single quotes or triple double qoutes")
+
+#few escape sequence
+print("This will\nprint in next line")
+print("This will create\t a tab space")
+print("This will create insert single \'qoutes\'")
+print("This will create insert double \"qoutes\"")
+print("This will insert \\backslash\\")
+print(r"This is raw string notation' \ ")
 
