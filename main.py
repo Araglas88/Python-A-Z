856f7b0a7ca01c721bc5d7a9355b9863d038eccf
Araglas88 <62518623+Araglas88@users.noreply.github.com>
2024-10-06 08:35:49 +0000

    commiting changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #import p001_escape_comments
 #import p002_variable
 #import p003_typecast
-import p004_input
+#import p004_input
+import p005_calculator
---
+++ p005_calculator.py
@@ -0,0 +1,15 @@
+"""
+This is a simple calculator program
+You can + - * / only 2 numbers
+"""
+
+num1 = input( "Enter first number: ")
+num2 = input( "Enter second number: ")
+
+print("Addition of " , num1 , " and " , num2 , " is " , int(num1) + int(num2))
+
+print("Subtraction of " , num1 , " and " , num2 , " is " , int(num1) - int(num2))
+
+print("Multiplication of " , num1 , " and " , num2 , " is " , int(num1) * int(num2))
+
+print("Division of " , num1 , " and " , num2 , " is " , int(num1) / int(num2))
